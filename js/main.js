63c02b8cd6cee226d6cc7cdc4f68173cc27acade
tati_sann <tanyawow376@gmail.com>
2020-10-16 11:18:51 +0400

    fix

--- js/main.js
+++ js/main.js
@@ -17,26 +17,26 @@ const CHECKS = [`12:00`, `13:00`, `14:00`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const DESCRIPTION = `Описание`;
 const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
-const locationX = {
-  min: 0,
-  max: map.offsetWidth
+const LocationX = {
+  MIN: 0,
+  MAX: map.offsetWidth
 };
-const locationY = {
-  min: 130,
-  max: 630
+const LocationY = {
+  MIN: 130,
+  MAX: 630
 };
-const price = {
-  min: 10000,
-  max: 100000
+const Price = {
+  MIN: 10000,
+  MAX: 100000
 };
-const rooms = {
-  min: 1,
-  max: 5
+const Rooms = {
+  MIN: 1,
+  MAX: 5
 };
 
-const guests = {
-  min: 0,
-  max: 5
+const Guests = {
+  MIN: 0,
+  MAX: 5
 };
 
 const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
